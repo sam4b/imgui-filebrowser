881ca59b82cc508c6c78a5f7cc8fd99aa9e2b461
Sam Barringer <92409647+sam4b@users.noreply.github.com>
2025-02-24 09:18:32 +0000

    Remove windows.h header pollution

--- imfilebrowser.h
+++ imfilebrowser.h
@@ -1310,25 +1310,4 @@ inline std::filesystem::path ImGui::FileBrowser::u8StrToPath(const char *str)
 #endif // #ifdef IMGUI_FILEBROWSER_UNDEF_NOMINMAX
 
 #endif // #ifdef _INC_WINDOWS
-
-inline std::uint32_t ImGui::FileBrowser::GetDrivesBitMask()
-{
-    const DWORD mask = GetLogicalDrives();
-    std::uint32_t ret = 0;
-    for(int i = 0; i < 26; ++i)
-    {
-        if(!(mask & (1 << i)))
-        {
-            continue;
-        }
-        const char rootName[4] = { static_cast<char>('A' + i), ':', '\\', '\0' };
-        const UINT type = GetDriveTypeA(rootName);
-        if(type == DRIVE_REMOVABLE || type == DRIVE_FIXED ||  type == DRIVE_REMOTE)
-        {
-            ret |= (1 << i);
-        }
-    }
-    return ret;
-}
-
 #endif
